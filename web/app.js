69ced8f2a87c35ac0db93bb9784b335ddd217914
MiguelRomeroGmx <contacto@miguelromero.mx>
2018-04-19 12:07:11 -0500

    envio de datos

--- web/app.js
+++ web/app.js
@@ -9,12 +9,31 @@ var config = {
   firebase.initializeApp(config);
 
   var database = firebase.database();
+
   var mostrarPing = document.getElementById("mostrarPing");
+  var datos = document.getElementById("datos");
 
   var ping = firebase.database().ref().child("ping");
+
+
+
 
   ping.on("value", function (snaptshot) {
       ping = snaptshot.val();
         mostrarPing.innerHTML = ping;
         console.log(ping);
+        
+        if (ping == 1) {
+      datos.innerHTML = "Alto";
+      console.log("alto");
+      
+  }
+  if (ping == 0) {
+      datos.innerHTML = "Bajo"; 
+      console.log("bajo");
+      
+  }
+
   });
+
+
